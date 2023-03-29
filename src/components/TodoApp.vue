b68cf213467317362ddadaa307c0d73dfd14e5f3
Asad javed <ajaved01@teresol.com>
2023-03-29 15:40:01 +0500

    Final Version

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -20,15 +20,29 @@
   </thead>
   <tbody>
     <tr v-for="(task,index) in tasks" :key="index">
-      <td>{{ task.name }}</td>
-      <td> {{ task.status }}</td>
       <td>
-    <div class="text-center">
+        <span :class="{'finished': task.status=='finished'}">
+          {{ task.name }}
+        </span>
+     </td>
+      <td style="width:120px"> 
+        <span @click="changeStatus(index)" class="pointer"
+        :class="{'text-danger': task.status=='to-do',
+      'text-warning': task.status=='in-progress',
+      'text-success': task.status=='finished'
+      }"
+        >
+
+          {{ firstCharUpper(task.status) }}
+        </span>
+      </td>
+      <td>
+    <div class="text-center" @click="editTask(index)">
     <span class="fa fa-pen"></span>
     </div>
       </td>
       <td>
-        <div class="text-center">
+        <div class="text-center" @click="deleteTask(index)">
     <span class="fa fa-trash"></span>
     </div>
       </td>
@@ -45,6 +59,8 @@ export default {
   data(){
     return {
       task:'',
+      editedTask:null,
+      availablestatuses:['to-do','in-progress','finished'],
       tasks:[
         {
           name:'Steal Fruits from Store',
@@ -59,18 +75,49 @@ export default {
   },
   methods:{
     submitTask(){
-      console.warn("button click");
+  
+
       if(this.task.length==0)
         return;
-      
+        if(this.editedTask==null){
       this.tasks.push({
         name:this.task,
         status:'to-do'
-      })
+      });
+         }else{
+      this.tasks[this.editedTask].name=this.task;
+      this.editedTask=null;
+    }
+      this.task='';
+    },
+    deleteTask(index){
+      this.tasks.splice(index,1)
+    },
+    editTask(index){
+      this.task=this.tasks[index].name;
+      this.editedTask=index;
+    },
+    changeStatus(index){
+      let newIndex=this.availablestatuses.indexOf(this.tasks[index].status);
+      if(++newIndex > 2)
+        newIndex=0;
+        this.tasks[index].status=this.availablestatuses[newIndex];
+      
+
+    },
+    firstCharUpper(str){
+      return str.charAt(0).toUpperCase()+ str.slice(1);
     }
   }
 
 }
 </script>
 
-
+<style scoped>
+.pointer{
+  cursor:pointer;
+}
+.finished{
+  text-decoration: line-through;
+}
+</style>
